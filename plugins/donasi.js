23a8d2b81ab3ebab7413b0f8620e88029dc64108
hafizhprjct <113893468+hafizhprjct@users.noreply.github.com>
2023-05-07 17:18:53 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 //Plugin By Xyzan!!
 let handler = async (m, { conn, usedPrefix }) => {
 	let url = 'https://photos.app.goo.gl/HtT2vwnGRRtU2zBE9.jpg'
-	conn.sendButton(m.chat, '〔 Dᴏɴᴛ ғᴏʀɢᴇᴛ Dᴏɴᴀᴛɪᴏɴ 〕                    \n╭─「_• *Dana/Gopay* •_」\n│ • *Dana* [08812258855]\n│ • *Gopay* [083127670609]\n╰────\n\n_◛˖ ```Ingin Donasi? Chat nomor owner```_', wm, await(await fetch(url)).buffer(), [['Owner',`/owner`]],m)
+	conn.sendButton(m.chat, '〔 Dᴏɴᴛ ғᴏʀɢᴇᴛ Dᴏɴᴀᴛɪᴏɴ 〕                    \n╭─「_• *Dana/Shopeepay* •_」\n│ • *Dana* [085651307830]\n│ • *ShopeePay* [085651307830]\n╰────\n\n_◛˖ ```Ingin Donasi? Chat nomor owner```_', wm, await(await fetch(url)).buffer(), [['Owner',`/owner`]],m)
 }
 handler.command = /^(donasi|dns)$/i
 handler.tags = ['info']
