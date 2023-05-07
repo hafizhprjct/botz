3eb99b149d211d350de0797a237987f71a057dd5
hafizhprjct <113893468+hafizhprjct@users.noreply.github.com>
2023-05-07 17:11:59 +0800

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 //Plugin By Xynoz!!
 let handler = async (m, { conn, usedPrefix }) => {
 	let url = 'https://telegra.ph/file/7e62b6510483b0abfde8a.jpg'
-	conn.sendButton(m.chat, ' *❲ ꜱᴇᴡᴀ ʙᴏᴛ ❳*\n\n⎇ 7 Hari 5ᴋ\n⎇ 30 Hari 15ᴋ\n⎇ 40 Hari 20ᴋ\n⎇ permanen 40k\n\n*ᴍɪɴᴀᴛ ᴄʜᴀᴛ ᴏᴡɴᴇʀ*', wm, await(await fetch(url)).buffer(), [['Owner',`/owner`]],m)
+	conn.sendButton(m.chat, ' *❲ Premium/Sewa BOT ❳*\n\n⎇ 7 Hari 5ᴋ\n⎇ 30 Hari 20ᴋ\n⎇ 40 Hari 30ᴋ\n⎇ permanen 40k\n\n*ᴍɪɴᴀᴛ ᴄʜᴀᴛ ᴏᴡɴᴇʀ*', wm, await(await fetch(url)).buffer(), [['Owner',`/owner`]],m)
 }
 handler.command = /^(sewa|sewabot)$/i
 handler.tags = ['info']
